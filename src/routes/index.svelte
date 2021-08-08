55be96b6d258d50bdb001d2e18849da698a5f27b
Christian Helbling <helbling@search.ch>
2021-08-08 11:07:32 +0200

    swisstopo vector map using maplibre-gl

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,42 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+
+import { onMount } from 'svelte';
+import { Map, GeolocateControl } from 'maplibre-gl'
+
+let mapDiv;
+
+onMount(() => {
+	const mapTilerKey = "get_your_own_OpIi9ZULNHzrESv6T2vL"
+	// at https://cloud.maptiler.com/account/keys/
+	const map = new Map({
+		container: mapDiv,
+		style: 'https://vectortiles.geo.admin.ch/styles/ch.swisstopo.leichte-basiskarte.vt/style.json',
+		center: [8.85, 47.49],
+		zoom: 15
+	})
+
+	map.addControl(
+	  new GeolocateControl({
+		positionOptions: {
+		  enableHighAccuracy: true
+		},
+	  })
+	)
+});
+</script>
+
+<svelte:head>
+	<link rel="stylesheet" href="/maplibre-gl.css" />
+</svelte:head>
+
+<style>
+	.map {
+	  position: absolute;
+	  top: 0;
+	  bottom: 0;
+	  left: 0;
+	  right: 0;
+	}
+</style>
+
+<div class="map" bind:this={mapDiv} />
