3899568b81d17db994f879aa90e61d1a70327b9a
Christian Helbling <helbling@search.ch>
2021-09-16 13:12:36 +0200

    generate average slope tiles for outer zoom levels as well

--- bin/mbtiles.sh
+++ bin/mbtiles.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-OUT=var/tlm_strasse_slope.mbtiles
+rm -f var/slope_exact.mbtiles
+ogr2ogr -dsco MINZOOM=12 -dsco MAXZOOM=18 -f MBTILES var/slope_exact.mbtiles PG:"dbname=downhillmap user=gisuser" tlm_strasse_slope_exact
 
-rm -f $OUT
+ogr2ogr -update -append -dsco MINZOOM=10 -dsco MAXZOOM=12 -f MBTILES var/tlm_strasse_slope.mbtiles PG:"dbname=downhillmap user=gisuser" tlm_strasse_slope_avg -clipdst 8.689674 47.462188 8.892401 47.53148
 
-ogr2ogr -dsco MINZOOM=12 -dsco MAXZOOM=18 -f MBTILES $OUT PG:"dbname=downhillmap user=gisuser" tlm_strasse_slope # -clipdst 8.689674 47.462188 8.892401 47.53148
+rm -f var/slope_avg.mbtiles
+ogr2ogr -dsco MINZOOM=8 -dsco MAXZOOM=14 -f MBTILES var/slope_avg.mbtiles PG:"dbname=downhillmap user=gisuser" tlm_strasse_slope_avg
--- bin/slope_table.sh
+++ bin/slope_table.sh
@@ -4,9 +4,9 @@ OTHERCOLS="uuid, objektart, belagsart, kunstbaute, name"
 
 psql -d downhillmap gisuser << EOT
 
-	DROP TABLE IF EXISTS tlm_strasse_slope;
+	DROP TABLE IF EXISTS tlm_strasse_slope_exact;
 
-	CREATE TABLE tlm_strasse_slope AS
+	CREATE TABLE tlm_strasse_slope_exact AS
 		SELECT
 			geom,
 			(st_z(st_startpoint(geom)) - st_z(st_endpoint(geom)))/st_length(geom::geography) as slope,
@@ -23,6 +23,28 @@ psql -d downhillmap gisuser << EOT
 			) a
 		) b;
 
-	CREATE INDEX ON tlm_strasse_slope using GIST(geom);
+	CREATE INDEX ON tlm_strasse_slope_exact using GIST(geom);
 EOT
 
+psql -d downhillmap gisuser << EOT
+	DROP TABLE IF EXISTS tlm_strasse_slope_avg;
+
+	CREATE TABLE tlm_strasse_slope_avg AS
+		SELECT
+			geom,
+			(st_z(st_startpoint(geom)) - st_z(st_endpoint(geom)))/st_length(geom::geography) as slope,
+			$OTHERCOLS
+		FROM (
+			SELECT
+				CASE WHEN st_z(st_startpoint(linestring)) < st_z(st_endpoint(linestring)) THEN St_Reverse(linestring) ELSE linestring END as geom,
+				$OTHERCOLS
+			FROM (
+				SELECT
+					(st_dump(geom)).geom as linestring,
+					$OTHERCOLS
+				FROM tlm_strasse_3d
+			) a
+		) b;
+
+	CREATE INDEX ON tlm_strasse_slope_avg using GIST(geom);
+EOT
